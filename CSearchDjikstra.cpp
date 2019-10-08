e420203ea95c6c7372671074793488a508b1c87e
Anis <anis_sg@hotmail.com>
2019-10-08 05:37:16 +0200

    - djikstra algo updated

--- CSearchDjikstra.cpp
+++ CSearchDjikstra.cpp
@@ -11,43 +11,82 @@ CSearchDjikstra::~CSearchDjikstra(void)
 }
 Triangle* CSearchDjikstra::GetSearchPath(Triangle* pTriangleInit, Triangle* pTriangleEnd)
 {
-	bool bFinded = false;
-	Triangle* pTriangleCurrent = pTriangleInit;
-	Triangle* pTriangleAnt = pTriangleInit;
-	pTriangleCurrent->SetVisited(true);
+    bool bFinded = false;
+    Point* pEnd = pTriangleEnd->GetMiddlePoint();
+    Triangle* pTriangleCurrent = pTriangleInit;
+    Triangle* pTriangleAnt = pTriangleInit;
+    vector<Triangle*> vNodesOpen;
+    vNodesOpen.push_back(pTriangleInit);
 
-	while( !bFinded)
-	{
-		bool bFindNeighbor = false;
-		for( int j=0; j<3; j++)
-		{
-			if( pTriangleCurrent->GetNeighbor(j) != NULL)
-			{
-				if( pTriangleCurrent->GetNeighbor(j)->IsInterior())
-				{
-					if( !pTriangleCurrent->GetNeighbor(j)->GetVisited())
-					{
-						if( pTriangleCurrent->GetNeighbor(j) == pTriangleEnd)
-						{
-							bFinded = true;
-						}
-						else
-						{
-							bFindNeighbor = true;
-						}
-						pTriangleAnt = pTriangleCurrent;
-						pTriangleCurrent = pTriangleCurrent->GetNeighbor(j);
-						pTriangleCurrent->_pPrevTriangle = pTriangleAnt;
-						pTriangleCurrent->SetVisited(true);
-						break;
-					}
-				}
-			}
-		}
-		if(!bFinded && !bFindNeighbor)
-		{
-			pTriangleCurrent = pTriangleCurrent->_pPrevTriangle;
-		}
-	}
-	return pTriangleCurrent;
+    float fLastCost = 0.0f;
+    vector<float> vCosts;
+    vCosts.push_back(fLastCost);
+
+
+
+    while (!bFinded)
+    {
+        ///check the best node from open list
+        float fMinDistance = 100000000.0f;
+        bool bFindNeighbor = false;
+        int iIndexMinDistance = -1;
+        for (int k = 0; k < vNodesOpen.size(); k++)
+        {
+            if (vNodesOpen[k] == pTriangleEnd)
+            {
+                bFinded = true;
+                pTriangleCurrent = vNodesOpen[k];
+            }
+            else
+            {
+                if (vCosts[k] < fMinDistance)
+                {
+                    fMinDistance = vCosts[k];
+                    iIndexMinDistance = k;
+                    bFindNeighbor = true;
+                }
+            }
+        }
+
+        if (!bFinded)
+        {
+            pTriangleCurrent = vNodesOpen[iIndexMinDistance];
+            for (int j = 0; j < 3; j++)
+            {
+                if (pTriangleCurrent->GetNeighbor(j) != NULL &&
+                    pTriangleCurrent->GetNeighbor(j)->IsInterior() &&
+                    pTriangleCurrent->GetNeighbor(j) != pTriangleAnt &&
+                    !pTriangleCurrent->GetNeighbor(j)->GetVisited()
+                    )
+                {
+                    Point* pMiddlePoint = pTriangleCurrent->GetNeighbor(j)->GetMiddlePoint();
+                    float fDistance = pMiddlePoint->DistanceToPoint(pEnd);
+
+                    bool bFound = false;
+                    for (int k = 0; k < vNodesOpen.size(); k++)
+                    {
+                        if (vNodesOpen[k] == pTriangleCurrent->GetNeighbor(j))
+                        {
+                            if (vCosts[k] > /*fMinDistance +*/ fDistance)
+                            {
+                                vNodesOpen[k] = pTriangleCurrent->GetNeighbor(j);
+                                vCosts[k] =/*fMinDistance +*/ fDistance;
+                                bFound = true;
+                            }
+                        }
+                    }
+                    if (!bFound)
+                    {
+                        pTriangleCurrent->GetNeighbor(j)->_pPrevTriangle = pTriangleCurrent;
+                        vNodesOpen.push_back(pTriangleCurrent->GetNeighbor(j));
+                        vCosts.push_back(/*fMinDistance + */fDistance);
+                    }
+                }
+            }
+            pTriangleCurrent->SetVisited(true);
+            vNodesOpen.erase(vNodesOpen.begin() + iIndexMinDistance);
+            vCosts.erase(vCosts.begin() + iIndexMinDistance);
+        }
+    }
+    return pTriangleCurrent;
 }
